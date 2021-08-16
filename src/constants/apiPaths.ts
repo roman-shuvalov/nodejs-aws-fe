d51070f82fcccd74a9092bca612250930c974f57
rshuvalov <rshuvalov@ukr.net>
2021-08-16 10:04:56 +0300

    update paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://9t2smluril.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://x4t5oybu59.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://2rjv6idkni.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://zuejyaeeb7.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
