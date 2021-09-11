fbda8949601bc8210f2d8ea313fc47e20e68ed99
rshuvalov <rshuvalov@ukr.net>
2021-09-11 21:38:33 +0300

    feat: add cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://2rjv6idkni.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://zuejyaeeb7.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://roman-shuvalov-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
