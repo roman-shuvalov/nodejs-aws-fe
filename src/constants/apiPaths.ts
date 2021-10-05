a3e5be8583a0d133b6f6b9ece33ce796de58fd64
rshuvalov <rshuvalov@ukr.net>
2021-10-05 23:07:06 +0300

    chore: add product url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://zuejyaeeb7.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://2rjv6idkni.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://zuejyaeeb7.execute-api.eu-west-1.amazonaws.com/dev',
